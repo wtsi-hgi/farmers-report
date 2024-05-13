c74cb090564c8d9fe09eeef60a5e42dcbbbbbcdb
enegoktan <64735857+enegoktan@users.noreply.github.com>
2024-05-13 10:51:11 +0100

    HSF-20: Investigating code coverage (#1)
    * modify read-me with run instructions
    
    * add coverage report script
    
    * minor changes
    
    ---------
    
    Co-authored-by: Iaroslav Popov <ip13@sanger.ac.uk>

--- README.md
+++ README.md
@@ -13,5 +13,11 @@ You can now knit report or serve an app.
 ## Development
 To run tests simply execute within an environment
 ```bash
-Rscript tests/testthat.R 
+Rscript tests/testthat.R
 ```
+
+To calculate the coverage
+```bash
+Rscript tests/covr.R
+```
+The coverage reports can be found in reports/ directory.
---
+++ tests/covr.R
@@ -0,0 +1,18 @@
+invisible(loadNamespace('covr'))
+
+src_to_exclude <- c("src/constants.R")
+
+src_code <- list.files("src", full.names = T)
+src_code <- setdiff(src_code, src_to_exclude)
+test_code <- list.files("tests/testthat", full.names = T)
+
+cov <- covr::file_coverage(source_files = src_code, test_files = test_code)
+dir.create("reports", showWarnings = F)
+
+for (src in src_code) {
+    outfile <- gsub(".R$", ".html", basename(src))
+    out_path <- file.path("reports", outfile)
+    covr::file_report(x = cov, file = src, out_file = out_path)
+}
+
+print(cov)
