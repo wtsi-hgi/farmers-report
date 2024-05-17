c10eb05ce05363a44dc009c1b8a8659972b2d318
ene goktan <goktan.ene@gmail.com>
2024-05-17 12:19:07 +0100

    refactor and test stat helpers
    Co-authored-by: Yaroslav <ya.popoff@gmail.com>

--- src/stat_helpers.R
+++ src/stat_helpers.R
@@ -5,50 +5,65 @@ source("src/elastic_helpers.R")
 source("src/table_helpers.R")
 
 get_user_statistics <- function (con, query) {
-  custom_aggs <- list(
-      "cpu_avail_sec" = build_elastic_sub_agg("AVAIL_CPU_TIME_SEC", "sum"),
-      "cpu_wasted_sec" = build_elastic_sub_agg("WASTED_CPU_SECONDS", "sum"),
-      "mem_avail_mb_sec" = build_elastic_sub_agg("MEM_REQUESTED_MB_SEC", "sum"),
-      "mem_wasted_mb_sec" = build_elastic_sub_agg("WASTED_MB_SECONDS", "sum"),
-      "wasted_cost" = wasted_cost_agg
-    )
-
-    b <- build_terms_query(
-      fields = c("NUM_EXEC_PROCS", "Job"),
-      aggs = custom_aggs,
-      query = query
-    )
-
-    res <- Search(con, index = index, body = b, asdf = T)
-
-    df <- parse_elastic_multi_agg(res, column_names = c('procs', 'job_status')) %>%
-      select(-doc_count)
-
-    dt <- generate_app_wastage_statistics(df)
-
-    dt_total <- generate_total_wastage_dt(dt)
-
-    dt <- rbind(dt, dt_total)
-
-    specify_wastage_reason(dt)
+  b <- build_user_statistics_query(query)
+  res <- Search(con, index = index, body = b, asdf = T)
+  dt <- generate_user_statistics(res)
 }
 
-get_team_statistics <- function (con, query) {
-  b <- list(query = query)
+build_bucket_aggregation_query <- function(fields, query) {
+  custom_aggs <- list(
+    "cpu_avail_sec" = build_elastic_sub_agg("AVAIL_CPU_TIME_SEC", "sum"),
+    "cpu_wasted_sec" = build_elastic_sub_agg("WASTED_CPU_SECONDS", "sum"),
+    "mem_avail_mb_sec" = build_elastic_sub_agg("MEM_REQUESTED_MB_SEC", "sum"),
+    "mem_wasted_mb_sec" = build_elastic_sub_agg("WASTED_MB_SECONDS", "sum"),
+    "wasted_cost" = wasted_cost_agg
+  )
 
+  b <- build_terms_query(
+    fields = fields,
+    aggs = custom_aggs,
+    query = query
+  )
+}
+
+build_user_statistics_query <- function(query) {
+  build_bucket_aggregation_query(
+    fields = c("NUM_EXEC_PROCS", "Job"),
+    query = query
+  )
+}
+
+generate_user_statistics <- function(res) {
+  df <- parse_elastic_multi_agg(res, column_names = c('procs', 'job_status')) %>%
+    select(-doc_count)
+
+  dt <- generate_app_wastage_statistics(df)
+
+  dt_total <- generate_total_wastage_dt(dt)
+
+  dt <- rbind(dt, dt_total)
+
+  specify_wastage_reason(dt)
+}
+
+get_team_statistics <- function(con, query) {
+  b <- list(query = query)
   res <- Search(
     con,
     index = index,
     time_scroll="1m",
     source = c('USER_NAME', 'Job',
-               'NUM_EXEC_PROCS', 'AVAIL_CPU_TIME_SEC', 'WASTED_CPU_SECONDS',
-               'MEM_REQUESTED_MB', 'MEM_REQUESTED_MB_SEC', 'WASTED_MB_SECONDS'),
+              'NUM_EXEC_PROCS', 'AVAIL_CPU_TIME_SEC', 'WASTED_CPU_SECONDS',
+              'MEM_REQUESTED_MB', 'MEM_REQUESTED_MB_SEC', 'WASTED_MB_SECONDS'),
     body = b,
     asdf = T,
     size = 10000
   )
-
   df <- pull_everything(con, res)
+  dt <- generate_team_statistics(df)
+}
+
+generate_team_statistics <- function (df) {
   df %>%
     rename(
       cpu_avail_sec = AVAIL_CPU_TIME_SEC,
@@ -68,31 +83,20 @@ get_team_statistics <- function (con, query) {
         wasted_cost = quote(sum(wasted_cost)),
         fail_rate = quote(sum(Job == 'Failed') / number_of_jobs)
       )
-    )
+    ) %>%
+    select(USER_NAME, number_of_jobs, fail_rate, cpu_avail_hrs,
+           cpu_wasted_hrs, cpu_wasted_frac, mem_avail_gb_hrs,
+           mem_wasted_gb_hrs, mem_wasted_frac, wasted_cost)
 }
 
-do_bom_aggregation <- function(con, query) {
-  custom_aggs <- list(
-    "cpu_avail_sec" = build_elastic_sub_agg("AVAIL_CPU_TIME_SEC", "sum"),
-    "cpu_wasted_sec" = build_elastic_sub_agg("WASTED_CPU_SECONDS", "sum"),
-    "mem_avail_mb_sec" = build_elastic_sub_agg("MEM_REQUESTED_MB_SEC", "sum"),
-    "mem_wasted_mb_sec" = build_elastic_sub_agg("WASTED_MB_SECONDS", "sum"),
-    "wasted_cost" = wasted_cost_agg
-  )
-
-  b <- build_terms_query(
+build_bom_aggregation <- function(query) {
+  build_bucket_aggregation_query(
     fields = c("ACCOUNTING_NAME", "NUM_EXEC_PROCS", "Job"),
-    aggs = custom_aggs,
     query = query
   )
-
-  res <- Search(con, index = index, body = b, asdf = T)
-
-  df <- parse_elastic_multi_agg(res, column_names = c('accounting_name', 'procs', 'job_status')) %>%
-    select(-doc_count)
 }
 
-do_bom_transformation <- function(df) {
+generate_bom_statistics <- function(df) {
   df %>%
     mutate(
       mem_wasted_cost = mem_wasted_mb_sec * ram_mb_second,
@@ -111,6 +115,11 @@ do_bom_transformation <- function(df) {
 }
 
 get_bom_statistics <- function (con, query) {
-  df <- do_bom_aggregation(con, query)
-  do_bom_transformation(df)
+  b <- build_bom_aggregation(query)
+  res <- Search(con, index = index, body = b, asdf = T)
+
+  df <- parse_elastic_multi_agg(res, column_names = c('accounting_name', 'procs', 'job_status')) %>%
+    select(-doc_count)
+
+  generate_bom_statistics(df)
 }
--- tests/testthat/test_stat_helpers.R
+++ tests/testthat/test_stat_helpers.R
@@ -10,9 +10,7 @@ if(basename(getwd()) == "testthat")
 source("src/stat_helpers.R")
 source("src/constants.R")
 
-test_that("do_bom_transformation works", {
-  local_edition(3)
-
+test_that("generate_bom_statistics works", {
   df <- data.frame(
     accounting_name = c('Team A', 'Team A', 'Team B'),
     cpu_avail_sec = c(300, 800, 1000),
@@ -24,9 +22,90 @@ test_that("do_bom_transformation works", {
     procs = c(1, 2, 6)
   )
 
-  dt <- do_bom_transformation(df)
+  dt <- generate_bom_statistics(df)
 
   expect_s3_class(dt,'data.frame')
-  expect_true('awesomeness' %in% names(dt))
-  expect_snapshot_value(dt, style = 'deparse')
+  expect_named(dt, c('accounting_name',
+                     'cpu_avail_hrs', 'cpu_wasted_hrs', 'cpu_wasted_frac',
+                     'mem_avail_gb_hrs', 'mem_wasted_gb_hrs', 'mem_wasted_frac', 
+                     'wasted_cost', 'awesomeness'))
+})
+
+test_that("build_user_statistics_query works", {
+  q <- list("bool" = list())
+  b <- build_user_statistics_query(q)
+
+  expect_type(b$aggs$stats$aggs, "list")
+  expect_named(b$aggs$stats$aggs, c('cpu_avail_sec', 'cpu_wasted_sec', 'mem_avail_mb_sec', 'mem_wasted_mb_sec', 'wasted_cost'))
+
+  expect_equal(
+    b$aggs$stats$multi_terms$terms, 
+    list(
+      list("field" = "NUM_EXEC_PROCS"), 
+      list("field" = "Job")
+    )
+  )
+})
+
+test_that("generate_user_statistics works", {
+  fake_elastic_response <- list(
+    aggregations = list(
+      stats = list(
+        buckets = tibble::tibble(
+          key = list(c("1", "Success"), c("2", "Failed")),
+          key_as_string = c('1|Success', '2|Failed'),
+          doc_count = c(10, 20),
+          mem_avail_mb_sec = c(500, 750),
+          mem_wasted_mb_sec = c(1000, 2000),
+          cpu_avail_sec = c (50, 75),
+          cpu_wasted_sec = c(100, 200),
+          wasted_cost = c(0.1, 0.2)
+        )
+      )
+    )
+  )
+
+  dt <- generate_user_statistics(fake_elastic_response)
+
+  expect_s3_class(dt, 'data.frame')
+  expect_named(dt, c('Reason', 'cpu_avail_hrs', 'cpu_wasted_hrs', 'cpu_wasted_frac', 
+                     'mem_avail_gb_hrs', 'mem_wasted_gb_hrs', 'mem_wasted_frac', 'wasted_cost'))
+  expect_true('Total' %in% dt$Reason)
+})
+
+test_that("generate_team_statistics works", {
+  fake_data_frame <- data.frame(
+    USER_NAME = c('user1', 'user2', 'user2'),
+    Job = c('Success', 'Success', 'Failed'),
+    NUM_EXEC_PROCS = c(1, 1, 3),
+    AVAIL_CPU_TIME_SEC = c(800, 1000, 1200),
+    WASTED_CPU_SECONDS = c(600, 500, 700),
+    MEM_REQUESTED_MB = c(1200, 2400, 3600), 
+    MEM_REQUESTED_MB_SEC = c(12000, 42000, 60000),
+    WASTED_MB_SECONDS = c(5000, 20000, 10000)
+  )
+
+  dt <- generate_team_statistics(fake_data_frame)
+  
+  expect_s3_class(dt, 'data.frame')
+  expect_named(dt, c('USER_NAME', 'number_of_jobs', 'fail_rate',
+                     'cpu_avail_hrs', 'cpu_wasted_hrs', 'cpu_wasted_frac',
+                     'mem_avail_gb_hrs', 'mem_wasted_gb_hrs', 'mem_wasted_frac', 'wasted_cost'))
+})
+
+test_that("build_bom_aggregation works", {
+  q <- list("bool" = list())
+  b <- build_bom_aggregation(q)
+
+  expect_type(b$aggs$stats$aggs, "list")
+  expect_named(b$aggs$stats$aggs, c('cpu_avail_sec', 'cpu_wasted_sec', 'mem_avail_mb_sec', 'mem_wasted_mb_sec', 'wasted_cost'))
+
+  expect_equal(
+    b$aggs$stats$multi_terms$terms, 
+    list(
+      list("field" = "ACCOUNTING_NAME"),
+      list("field" = "NUM_EXEC_PROCS"), 
+      list("field" = "Job")
+    )
+  )
 })
