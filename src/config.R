fc7210187558ca3e64df169f2718f122a7bd956f
Iaroslav Popov <ip13@sanger.ac.uk>
2024-06-25 18:25:55 +0100

    move port and index to the config

--- src/config.R
+++ src/config.R
@@ -6,7 +6,7 @@ read_config <- function(config_file = "config.yaml") {
     
     config <- yaml.load_file(config_file)
     if ("elastic" %in% names(config))
-        if (all(c("host", "username", "password") %in% names(config$elastic)))
+        if (all(c("host", "username", "password", "port", "index") %in% names(config$elastic)))
             return (config)
     
     stop("No Elastic credentials found in the config file!")
