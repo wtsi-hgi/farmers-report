a4826397147ce9483ce7fac2a36d0766d7661205
Iaroslav Popov <ip13@sanger.ac.uk>
2024-06-26 14:25:26 +0100

    rewrite elastic scroll
    now we iterate until we reach total number of records instead of empty response

--- src/elastic_helpers.R
+++ src/elastic_helpers.R
@@ -134,15 +134,12 @@ extract_hits_from_elastic_response <- function(x) {
 
 pull_everything <- function(connection, response) {
   dt <- extract_hits_from_elastic_response(response)
-  hits <- 1
+  total_hits <- response$hits$total$value
 
-  while(hits != 0){
-    res <- scroll(connection, response$`_scroll_id`, asdf = T)
-    hits <- length(res$hits$hits)
-    if(hits > 0){
-      df <- extract_hits_from_elastic_response(res)
-      dt <- rbind(dt, df)
-    }
+  while(nrow(dt) < total_hits){
+    response <- scroll(connection, response$`_scroll_id`, asdf = T)
+    df <- extract_hits_from_elastic_response(response)
+    dt <- rbind(dt, df)
   }
 
   scroll_clear(connection, response$`_scroll_id`)
